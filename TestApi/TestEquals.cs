ba0061e5cece675b4f3cba725e52583a06b25dfd
Giulio Bacchilega <gbacchilega@mydev.it>
2022-04-04 16:24:09 +0200

    Added tests

---
+++ TestApi/TestEquals.cs
@@ -0,0 +1,21 @@
+using Xunit;
+
+namespace TestApi
+{
+    public class TestEquals
+    {
+
+
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 == 1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(1 == 0);
+        }
+    }
+}
